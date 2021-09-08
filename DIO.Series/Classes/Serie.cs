55bcf0d4b3e3b3434b2c77ccb47b6361c8e62bff
Breno <bor@cin.ufpe.br>
2021-09-08 11:38:38 -0300

    Setting class Serie

--- DIO.Series/Classes/Serie.cs
+++ DIO.Series/Classes/Serie.cs
@@ -1,7 +1,44 @@
+using System;
+
 namespace DIO.Series
 {
     public class Serie : EntidadeBase
     {
-        
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += "Gênero: " + this.Genero + Environment.NewLine;
+            result += "Título: " + this.Titulo + Environment.NewLine;
+            result += "Descrição: " + this.Descricao + Environment.NewLine;
+            result += "Ano: " + this.Ano;
+            return result;
+        }
+
+        public string getTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int getID()
+        {
+            return this.id;
+        }
     }
 }
