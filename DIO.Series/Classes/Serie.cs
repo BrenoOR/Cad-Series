e052c782c32855f221cf517b2c2f70ad29676438
Breno <bor@cin.ufpe.br>
2021-09-11 18:22:07 -0300

    Updating Serie.cs, Program.cs and .gitignore

--- DIO.Series/Classes/Serie.cs
+++ DIO.Series/Classes/Serie.cs
@@ -12,6 +12,8 @@ namespace DIO.Series
 
         private int Ano { get; set; }
 
+        private bool Excluido { get; set; }
+
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
             this.id = id;
@@ -19,6 +21,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -39,6 +42,11 @@ namespace DIO.Series
         public int getID()
         {
             return this.id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
         }
     }
 }
--- DIO.Series/Program.cs
+++ DIO.Series/Program.cs
@@ -4,10 +4,106 @@ namespace DIO.Series
 {
     class Program
     {
+        static SerieRepo repo = new SerieRepo();
         static void Main(string[] args)
         {
-            Serie entidade = new Serie();
-            Console.WriteLine("Hello World!");
+            string option = getOpcaoUsuario();
+
+            while (option.ToUpper() != "X")
+            {
+                switch(option)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        //AtualizarSerie();
+                        break;
+                    case "4":
+                        //ExcluirSerie();
+                        break;
+                    case "5":
+                        //VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                option = getOpcaoUsuario();
+            }
+        }
+
+        private static void InserirSerie()
+        {
+            Console.WriteLine("Inserir nova série:");
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.WriteLine("Escolha o gênero entre as opções acima: ");
+            int inputGenero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o título da série: ");
+            string inputTitulo = Console.ReadLine();
+
+            Console.WriteLine("Digite o ano de lançamento da série: ");
+            int inputAno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva a sinopse da série: ");
+            string inputDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: repo.ProximoID(),
+                                        genero: (Genero)inputGenero,
+                                        titulo: inputTitulo,
+                                        ano: inputAno,
+                                        descricao: inputDescricao);
+            
+            repo.Insere(novaSerie);
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar séries:");
+
+            var lista = repo.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach(var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1}", serie.getID(), serie.getTitulo());
+            }
+        }
+
+        private static string getOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bem vindo à DIO Séries!!!");
+            Console.WriteLine("O que desejas?");
+
+            Console.WriteLine("1 - Listar Séries");
+            Console.WriteLine("2 - Inserir nova Série");
+            Console.WriteLine("3 - Atualizar Série");
+            Console.WriteLine("4 - Excluir Série");
+            Console.WriteLine("5 - Visualizar Série");
+            Console.WriteLine("C - Limpar a tela");
+            Console.WriteLine("X - Sair");
+
+            string opcao = Console.ReadLine().ToUpper();            
+            Console.WriteLine();
+            return opcao;
         }
     }
 }
